1781811dc3367b9de4d4d0ff96ee91b676e95b03
Alexey Shvets <alexey199822la@gmail.com>
2023-09-23 11:00:01 +0300

    dev/app path modal proto ui created

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,1 +1,2 @@
 export * from './OpenAddPathModalButton';
+export * from './AddPathButton';
--- src/components/organisms/AddPathModal.tsx
+++ src/components/organisms/AddPathModal.tsx
@@ -5,20 +5,85 @@ import {
   ModalContent,
   ModalHeader,
   ModalCloseButton,
+  ModalBody,
+  HStack,
+  VStack,
+  FormControl,
 } from '@chakra-ui/react';
-import { useModalsOpenSlice } from 'src/hooks';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { AddPathButton, CustomInput } from 'src/components';
+import {
+  useFormValidators,
+  useModalsOpenSlice,
+  usePathesSlice,
+} from 'src/hooks';
+
+interface FormValues {
+  title: string;
+  shortDescription?: string;
+  fullDescription?: string;
+}
 
 export const AddPathModal: React.FC = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<FormValues>();
+  const { setMaxLength, fieldRequiredMessage } = useFormValidators();
+  const { addPath } = usePathesSlice();
   const { addPathModal } = useModalsOpenSlice();
   const { isOpen, close } = addPathModal;
 
+  const titleRegister = {
+    ...register('title', { required: fieldRequiredMessage }),
+  };
+  const shortDescriptionRegister = {
+    ...register('shortDescription', { maxLength: setMaxLength(160) }),
+  };
+  const fullDescriptionRegister = {
+    ...register('fullDescription'),
+  };
+
+  const onSubmit: SubmitHandler<FormValues> = data => {
+    addPath(data);
+    close();
+  };
+
   return (
-    <Modal isOpen={isOpen} onClose={close}>
+    <Modal size="xl" isOpen={isOpen} onClose={close}>
       <ModalOverlay />
 
       <ModalContent>
         <ModalHeader>Add new path</ModalHeader>
         <ModalCloseButton />
+
+        <ModalBody>
+          <HStack flex={1}>
+            <FormControl onSubmit={handleSubmit(onSubmit)}>
+              <VStack>
+                <CustomInput
+                  title="Title"
+                  register={titleRegister}
+                  errorText={errors.title?.message as string}
+                />
+                <CustomInput
+                  title="Short description"
+                  type="textarea"
+                  register={shortDescriptionRegister}
+                  errorText={errors.shortDescription?.message as string}
+                />
+                <CustomInput
+                  title="Full description"
+                  type="textarea"
+                  register={fullDescriptionRegister}
+                  errorText={errors.fullDescription?.message as string}
+                />
+                <AddPathButton onClick={handleSubmit(onSubmit)} />
+              </VStack>
+            </FormControl>
+          </HStack>
+        </ModalBody>
       </ModalContent>
     </Modal>
   );
