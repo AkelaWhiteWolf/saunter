82143becb9c93ffa4d331b18a893069ea2087dea
Alexey Shvets <alexey199822la@gmail.com>
2023-09-25 13:51:54 +0300

    dev/markers added to type of pathes

--- src/types/pathesSliceTypes.tsx
+++ src/types/pathesSliceTypes.tsx
@@ -1,9 +1,15 @@
+export interface PathMarkerType {
+  lat?: number;
+  lng?: number;
+}
+
 export interface PathSliceType {
   id: number;
   title: string;
   shortDescription?: string;
   fullDescription?: string;
   isFavorite?: boolean;
+  markers: PathMarkerType[];
 }
 
 export type AddPathToSliceType = Omit<PathSliceType, 'id' | 'isFavorite'>;
