dd6ecb09140fc746266606673a8d84d9a1d33290
Alexey Shvets <alexey199822la@gmail.com>
2023-09-27 10:56:53 +0300

    dev/pathes from Firebase added to Redux automatically

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { PathesFullContainer } from 'src/components';
+import { useFirestoreDB, usePathesSlice } from 'src/hooks';
 
 export const MainPage: React.FC = () => {
+  const { getPathesFromDB } = useFirestoreDB();
+  const { addReadyPathes } = usePathesSlice();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const dbPathes = await getPathesFromDB();
+      console.log('dbPathes', dbPathes);
+      if (dbPathes?.length) {
+        addReadyPathes(dbPathes);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return <PathesFullContainer />;
 };
